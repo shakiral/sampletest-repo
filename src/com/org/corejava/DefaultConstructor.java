01fbfda7c73a170f8d705fe021db3def0c055436
Shakirabanu Lattannavar <shakirabanu.lattannavar@QUALITEST.LOCAL>
2019-07-31 17:12:13 +0530

    moved changed to my branch

--- src/com/org/corejava/DefaultConstructor.java
+++ src/com/org/corejava/DefaultConstructor.java
@@ -2,7 +2,7 @@ package com.org.corejava;
 
 class DefaultConstructor{  
 	//creating a default constructor  
-	DefaultConstructor(){System.out.println("Bike is created");}  
+	DefaultConstructor(){System.out.println("Bike is created only");}  
 	//main method  
 	public static void main(String args[]){  
 	//calling a default constructor  
